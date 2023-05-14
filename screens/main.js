ea97dbfc3923b8c36f9d6fff47136d3aa7d1b948
Godwin Burume <godwinnyembo@gmail.com>
2023-05-14 13:25:05 +0100

    edit homepage

--- screens/main.js
+++ screens/main.js
@@ -6,10 +6,11 @@ import {
   ScrollView,
   TouchableOpacity,
   Text,
+  Dimensions,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
-import { SearchBar } from "react-native-elements";
+import { Card, SearchBar, Button, Badge } from "react-native-elements";
 import HouseList from "./ListItem";
 
 const MyComponent = () => {
@@ -26,6 +27,8 @@ const MyComponent = () => {
         "Experience the vibrant energy of this stunning Red House, the perfect blend of style and sophistication. With spacious rooms and modern amenities, you'll love calling this place home.",
       latitude: -4.325,
       longitude: 15.322,
+      type: "rent",
+      commune: "Ngaliema",
       price: "$55K",
     },
     {
@@ -36,6 +39,8 @@ const MyComponent = () => {
         "Imagine waking up to the breathtaking views of fall foliage every morning! This cozy Fall House is the perfect place to call home during the crisp autumn months.",
       latitude: -4.329,
       longitude: 15.324,
+      type: "sell",
+      commune: "Gombe",
       price: "$95K",
     },
     {
@@ -46,6 +51,8 @@ const MyComponent = () => {
         "Step back in time with this charming Old House, full of character and history. With unique architectural features and modern updates, you'll enjoy the best of both worlds.",
       latitude: -4.331,
       longitude: 15.326,
+      type: "rent",
+      commune: "Maluku",
       price: "$155K",
     },
     {
@@ -56,6 +63,8 @@ const MyComponent = () => {
         "Take a dip in your own private oasis with this luxurious Pool House. Perfect for entertaining or just relaxing on a sunny day, you won't want to leave this stunning property.",
       latitude: -4.333,
       longitude: 15.328,
+      type: "rent",
+      commune: "Kinshasa",
       price: "$355K",
     },
     {
@@ -66,6 +75,8 @@ const MyComponent = () => {
         "Escape to this serene White House, a peaceful retreat from the hustle and bustle of everyday life. With a bright and airy interior and gorgeous outdoor spaces, you'll feel at home the moment you step inside.",
       latitude: -4.335,
       longitude: 15.33,
+      type: "sell",
+      commune: "Ngaliema",
       price: "$555K",
     },
     {
@@ -76,6 +87,8 @@ const MyComponent = () => {
         "Experience the ultimate island lifestyle with this breathtaking Bahamas House. With stunning ocean views and luxurious amenities, you'll feel like you're on vacation every day.",
       latitude: -4.337,
       longitude: 15.332,
+      type: "rent",
+      commune: "Ngaliema",
       price: "$655K",
     },
     {
@@ -86,6 +99,8 @@ const MyComponent = () => {
         "Live in luxury with this gorgeous Red House, the perfect blend of elegance and comfort. With spacious rooms and high-end finishes, you'll love entertaining friends and family in this stunning property.",
       latitude: -4.339,
       longitude: 15.334,
+      type: "sell",
+      commune: "Ngaliema",
       price: "$455K",
     },
     {
@@ -96,6 +111,8 @@ const MyComponent = () => {
         "Wake up to paradise every day with this stunning Bahamas House. With access to pristine beaches and world-class amenities, you'll never want to leave this slice of heaven.",
       latitude: -4.341,
       longitude: 15.336,
+      type: "rent",
+      commune: "Gombe",
       price: "$425K",
     },
     {
@@ -106,6 +123,8 @@ const MyComponent = () => {
         "Enjoy the ultimate in luxury living with this magnificent Pool House. With a gorgeous outdoor pool and beautifully appointed interior, this property is the perfect place to call home.",
       latitude: -4.343,
       longitude: 15.335,
+      type: "rent",
+      commune: "Ngaliema",
       price: "$809K",
     },
   ];
@@ -127,18 +146,35 @@ const MyComponent = () => {
       />
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         {houses.map((house) => (
-          <View style={styles.imageContainer}>
-            <TouchableOpacity
-              key={house.id}
-              onPress={() => handleImageClick(house.id)}
-            >
+          <TouchableOpacity
+            key={house.id}
+            onPress={() => handleImageClick(house.id)}
+            style={styles.card}
+          >
+            <Card containerStyle={styles.cardContent}>
               <Image source={house.source} style={styles.image} />
-              <Text style={styles.caption}>{house.title}</Text>
-              <Text numberOfLines={2} style={styles.description}>
-                {house.sellingCaption}
-              </Text>
-            </TouchableOpacity>
-          </View>
+              <View style={styles.cardText}>
+                <Text style={styles.caption}>
+                  {house.title}{" "}
+                  <Badge
+                    value={`• ${house.type === "sell" ? "A vendre" : "A louer"} •`}
+                    badgeStyle={{ backgroundColor: house.type === "sell" ?"#d4edda" : "#d1ecf1" }}
+                    textStyle={{ color: house.type === "sell" ? "#155724" : "#0c5460" }}
+                  />{" "}
+                  <Badge
+                    value={`• ${house.commune} •`}
+                    badgeStyle={{ backgroundColor: "#d6d8d9" }}
+                    textStyle={{ color: "#1b1e21" }}
+                  />
+                </Text>
+
+                <Text numberOfLines={2} style={styles.description}>
+                  {house.sellingCaption}
+                </Text>
+              </View>
+              <Button style={styles.button} title={"Nous contacter"} />
+            </Card>
+          </TouchableOpacity>
         ))}
       </ScrollView>
     </View>
@@ -163,25 +199,45 @@ const styles = StyleSheet.create({
   },
   scrollContainer: {
     padding: 10,
+    alignItems: "center",
   },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginBottom: 10,
   },
-  imageContainer: {
+  card: {
     marginBottom: 10,
+    width: Dimensions.get("window").width * 0.95,
+  },
+  cardText: {
+    paddingHorizontal: 5,
+  },
+  button: {
+    paddingHorizontal: 5,
+    marginBottom: 0,
+  },
+  cardContent: {
+    borderRadius: 5,
+    margin: 0,
+    paddingTop: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    overflow: "hidden",
+    width: Dimensions.get("window").width * 0.95,
+    paddingBottom: 5,
   },
   image: {
-    width: "100%",
-    height: 300,
+    width: Dimensions.get("window").width * 0.95,
+    height: Dimensions.get("window").width * 0.95,
     resizeMode: "cover",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
   },
   caption: {
     marginTop: 10,
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center",
   },
   showMore: {
     color: "blue",
@@ -189,7 +245,8 @@ const styles = StyleSheet.create({
     textAlign: "right",
   },
   description: {
-    marginTop: 10,
+    marginTop: 5,
+    marginBottom: 8,
     fontSize: 16,
   },
 });
